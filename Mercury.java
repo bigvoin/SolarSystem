8848b630224c552a95922ea6860b090a330e492a
Nikolay Gachev <48036296+bigvoin@users.noreply.github.com>
2019-10-24 17:35:03 +0100

    not useful for the program

--- Mercury.java
+++
@@ -1,7 +0,0 @@
-public class Mercury extends Planets
-{
-    public Mercury(String name, double distance, double angle, double diameter, String colour, double velocity)
-    {
-        super(name,distance,angle,diameter,colour,velocity);
-    }
-}
